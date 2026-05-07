8d0bbcdd6b6a7c1e2d6cfb1f1fcdc937739a7f29
KachowM80 <24463@burnside.school.nz>
2026-05-08 09:51:14 +1200

    tuff

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
-print("hello, world!")
+import sqlite3
+
+DATABASE = 'fighter.db'
+
+db = sqlite3.connect(DATABASE)
+cursor = db.cursor()
+sql = "SELECT * FROM fighter;"
+cursor.execute(sql)
+result = cursor.fetchall()
+print(result)
+db.close()
