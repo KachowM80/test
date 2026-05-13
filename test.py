158f11c33706c10ad5db88576dd47cd389c156dc
KachowM80 <24463@burnside.school.nz>
2026-05-13 14:12:31 +1200

    tuff

--- test.py
+++ test.py
@@ -1,11 +1,23 @@
 import sqlite3
 
+#constants and variables
 DATABASE = 'fighter.db'
 
-db = sqlite3.connect(DATABASE)
-cursor = db.cursor()
-sql = "SELECT * FROM fighter;"
-cursor.execute(sql)
-result = cursor.fetchall()
-print(result)
-db.close()
+
+#functions
+def print_all_aircraft():
+    '''print all them tuffly'''
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT * FROM fighter;"
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    #loop
+    for fighter in result:
+        print(f"{fighter[1]}{fighter[2]}{fighter[3]}{fighter[4]}{fighter[5]}{fighter[6]}")
+    #loop done
+    db.close()
+
+
+#main code
+print_all_aircraft()
