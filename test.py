117d82352d35f4496b31d3e41200c050c8487685
KachowM80 <24463@burnside.school.nz>
2026-05-18 12:52:10 +1200

    very tufgf

--- test.py
+++ test.py
@@ -13,6 +13,7 @@ def print_all_aircraft():
     cursor.execute(sql)
     result = cursor.fetchall()
     #loop
+    print(f"name                          speed    max_g climb range payload")
     for fighter in result:
         print(f"{fighter[1]:<30}{fighter[2]:<8}{fighter[3]:<6}{fighter[4]:<6}{fighter[5]:<6}{fighter[6]:<6}")
     #loop done
@@ -20,4 +21,27 @@ def print_all_aircraft():
 
 
 #main code
-print_all_aircraft()
+while True:
+    user_input = input(
+"""
+What would you like to do.
+1. Print all aircraft
+2. Print all aircraft sorted by speed
+7. Exit
+""")
+    if user_input == "1":
+        print_all_aircraft()
+    elif user_input == "2":
+        print_all_aircraft_by_speed()
+    elif user_input == "3":
+        print_all_aircraft_by_climb()
+    elif user_input == "4":
+        print_all_aircraft_by_range()
+    elif user_input == "5":
+        print_all_aircraft_by_climb()
+    elif user_input == "6":
+        print_all_aircraft_by_payload()
+    elif user_input == "7":
+        break
+    else:
+        print('That was not an option\n')
